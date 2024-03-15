f2f303070458aeeeba0f85dbc569b5dbf54e4023
SHAKTHI-VEL <shakthivel28203@gmail.com>
2024-03-15 23:57:10 +0530

    api added for gaining list of all food and activity

--- routes/getFootprint.js
+++ routes/getFootprint.js
@@ -4,6 +4,8 @@ const searchActivity = require('../controller/searchActivity')
 const addData = require('../controller/addUserData')
 const getUserData = require('../controller/getUserData')
 const getUserDataofToday = require('../controller/getUserDataofToday')
+const getAllFood = require('../controller/getAllFood')
+const getAllActivity = require('../controller/getAllActivity')
 const router=express.Router()
 
 router.get('/food',searchFood)
@@ -11,5 +13,7 @@ router.get('/activity',searchActivity)
 router.post('/add',addData)
 router.get('/user/:uid',getUserData)
 router.get('/user/today/:uid',getUserDataofToday)
+router.get('/getallfood',getAllFood)
+router.get('/getallactivity',getAllActivity)
 
 module.exports=router
