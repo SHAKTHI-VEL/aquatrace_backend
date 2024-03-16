b2fdb257eca0711f204d38306e5e9662d5e0c845
SHAKTHI-VEL <shakthivel28203@gmail.com>
2024-03-16 14:42:26 +0530

    added waterfootprint

--- controller/addUserData.js
+++ controller/addUserData.js
@@ -3,13 +3,76 @@ const prisma=new PrismaClient()
 
 const addData=async (req,res)=>{
     try {
-        const{uid,item,waterfootprint,quantity,measure}=req.body;
-        const result=await prisma.userdata.create({
-            data:{
-                uid,item,waterfootprint,quantity,measure
+        const{uid,item,quantity,measure}=req.body;
+        if(measure==='mins'){
+            const findWaterfootprint=await prisma.activity.findFirst({
+                where:{
+                    activity:item
+                }
+            })
+            const result=await prisma.userdata.create({
+                data:{
+                    uid,item,quantity,measure,waterfootprint:findWaterfootprint.waterfootprint*quantity
+                }
+            })
+            return res.status(200).json({success:true,result})
+        }
+        else{
+            if(measure==grams){
+                const findWaterfootprint=await prisma.food.findFirst({
+                    where:{
+                        fooditems:item
+                    }
+                })
+                const result=await prisma.userdata.create({
+                    data:{
+                        uid,item,quantity,measure,waterfootprint:findWaterfootprint.waterfootprint*quantity
+                    }
+                })
+                return res.status(200).json({success:true,result})
             }
-        })
-        return res.status(200).json({success:true,result})
+            if(measure==katori){
+                const findWaterfootprint=await prisma.food.findFirst({
+                    where:{
+                        fooditems:item
+                    }
+                })
+                const result=await prisma.userdata.create({
+                    data:{
+                        uid,item,quantity,measure,waterfootprint:findWaterfootprint.waterfootprint*quantity*124
+                    }
+                })
+                return res.status(200).json({success:true,result})
+            }
+            if(measure==cup){
+                const findWaterfootprint=await prisma.food.findFirst({
+                    where:{
+                        fooditems:item
+                    }
+                })
+                const result=await prisma.userdata.create({
+                    data:{
+                        uid,item,quantity,measure,waterfootprint:findWaterfootprint.waterfootprint*quantity*206
+                    }
+                })
+                return res.status(200).json({success:true,result})
+            }
+
+            if(measure==bowl){
+                const findWaterfootprint=await prisma.food.findFirst({
+                    where:{
+                        fooditems:item
+                    }
+                })
+                const result=await prisma.userdata.create({
+                    data:{
+                        uid,item,quantity,measure,waterfootprint:findWaterfootprint.waterfootprint*quantity*290
+                    }
+                })
+                return res.status(200).json({success:true,result})
+            }
+        }
+        
     } catch (error) {
         console.log(error);
         return res.status(400).json({success:false,message:error})
