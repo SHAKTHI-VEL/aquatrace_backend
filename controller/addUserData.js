84f2aebc84bc3e04f6e48b84f91eb7786e76f484
SHAKTHI-VEL <shakthivel28203@gmail.com>
2024-04-03 19:16:30 +0530

    xp

--- controller/addUserData.js
+++ controller/addUserData.js
@@ -31,7 +31,7 @@ const addData=async (req,res)=>{
                         uid,item,quantity,measure,waterfootprint:findWaterfootprint.waterfootprint*quantity
                     }
                 })
-               
+               await addXp(findWaterfootprint.waterfootprint,uid)
                 return res.status(200).json({success:true,result})
             }
             if(measure==="katori"){
@@ -45,7 +45,7 @@ const addData=async (req,res)=>{
                         uid,item,quantity,measure,waterfootprint:findWaterfootprint.waterfootprint*quantity*124
                     }
                 })
-                
+                await addXp(findWaterfootprint.waterfootprint,uid)
                 return res.status(200).json({success:true,result})
             }
             if(measure==="cup"){
@@ -59,7 +59,7 @@ const addData=async (req,res)=>{
                         uid,item,quantity,measure,waterfootprint:findWaterfootprint.waterfootprint*quantity*206
                     }
                 })
-              
+                await addXp(findWaterfootprint.waterfootprint,uid)
                 return res.status(200).json({success:true,result})
             }
 
@@ -74,7 +74,7 @@ const addData=async (req,res)=>{
                         uid,item,quantity,measure,waterfootprint:findWaterfootprint.waterfootprint*quantity*290
                     }
                 })
-             
+                await addXp(findWaterfootprint.waterfootprint,uid)
                 return res.status(200).json({success:true,result})
             }
 
@@ -89,7 +89,7 @@ const addData=async (req,res)=>{
                         uid,item,quantity,measure,waterfootprint:findWaterfootprint.waterfootprint*quantity*100
                     }
                 })
-                
+                await addXp(findWaterfootprint.waterfootprint,uid)
                 return res.status(200).json({success:true,result})
             }
         }
